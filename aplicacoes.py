1fb9e8f98179915d4be1e4beb5a1433ba2ab567f
Kauanny19 <kauannycristina01n@gmail.com>
2024-06-03 11:32:07 -0300

    Aula 03-06

--- aplicacoes.py
+++ aplicacoes.py
@@ -9,4 +9,36 @@ for i in range(1, len(lista)):
         maior_numero = lista[i]
 print("O Maior número da lista é:", maior_numero)
 
+#Exemplo2
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 18]
+maior = my_list[0]
+for i in my_list:
+    if i > maior:
+        maior = i 
+print("Maior valor 2:", maior)
+
+#Exemplo 3
+ultima_lista = my_list[:]
+mel = ultima_lista[0]
+for i in ultima_lista[1:]:
+    if i > mel:
+        mel = i
+print("Maior valor 3:", mel)
+
+
+#Encontrar a localização de um determinado elemento dentro de uma lista
+frutas = ["abacaxi", "maçã", "pêra", "mamão", "uva", "melancia"]
+elemento = "melancia"
+achado = False
+
+for i in range(len(frutas)):
+    achado = frutas[i] == elemento
+    if achado:
+        break
+
+if achado:
+    print("Elemento encontrado no índice:", i)
+else:
+    print("NÃO ENCONTRADO!!!")
+    
 time.sleep(3)
